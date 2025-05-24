2920a809d3eb73d268cab7e7b684030b28ec06ff
xlsrln <31051075+xlsrln@users.noreply.github.com>
2025-05-24 19:30:07 +0200

    try fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -6,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // Added for GitHub Pages deployment!!
+  base: process.env.NODE_ENV === 'production' ? '/cat-results-page/' : '/',
   server: {
     host: "::",
     port: 8080,
@@ -20,5 +19,8 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 }));
