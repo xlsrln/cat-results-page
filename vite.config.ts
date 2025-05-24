44dec4df836bff8cf217c5b5ce68f6cd6e25471c
xlsrln <31051075+xlsrln@users.noreply.github.com>
2025-05-24 23:29:38 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import ghPages from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,6 +15,7 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    ghPages(),
   ].filter(Boolean),
   resolve: {
     alias: {
