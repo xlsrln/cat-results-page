943f739218f4d170497c2ec5a7184d13115903c2
xlsrln <31051075+xlsrln@users.noreply.github.com>
2025-05-24 20:50:04 +0200

    try again

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -6,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/cat-results-page/", // Added for GitHub Pages deployment!!
+  base: mode === 'production' ? "/cat-results-page/" : "/",
   server: {
     host: "::",
     port: 8080,
